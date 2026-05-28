0ed0d47b4c3f04c9a298ffc941b31a91f0f3e131
EvaPikal <pikalovaeva@seznam.cz>
2026-05-28 20:48:10 +0200

    Traffic Light
    Simple Python project simulating a traffic light system using basic conditions and user input.

---
+++ my-calculator/Semafor.py
@@ -0,0 +1,15 @@
+print("Semafor")
+
+color = input("Zadej barvu semaforu (červená, oranžová, zelená): ")
+
+if color == "červená":
+    print("Stůj!")
+
+elif color == "oranžová":
+    print("Připrav se!")
+
+elif color == "zelená":
+    print("Jeď!")
+
+else:
+    print("Neznámá barva.")
